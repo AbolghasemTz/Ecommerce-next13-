b7aaeb0a36a1c3d15c50fe523220bf4217d3b150
Abolghasem Taziki <113670236+AbolghasemTz@users.noreply.github.com>
2023-08-14 09:54:29 +0330

    coustom hooks

---
+++ src/app/constant/tableHeads.js
@@ -0,0 +1,42 @@
+export const userPaymentTHeads = [
+    {id:1,label:"#"},
+    {id:2,label:"شماره فاکتور"},
+    {id:3,label:"توضیحات "},
+    {id:4,label:"محصولات"},
+    {id:5,label:"مبلغ "},
+    {id:6,label:"تاریخ "},
+    {id:7,label:"وضعیت "},
+]
+export const userListTabelHeads = [
+    {id:1,label:"#"},
+    {id:2,label:"نام"},
+    {id:3,label:"ایمیل "},
+    {id:4,label:"شماره موبایل"},
+    {id:5,label:"محصولات "},
+    {id:6,label:"تاریخ پیوستن "},
+    {id:7,label:"مشاهده "},
+ 
+]
+export const productTabelTHead = [
+    {id:1,label:"#"},
+    {id:2,label:"عنوان"},
+    {id:3,label:"دسته بندی "},
+    {id:4,label:"قیمت "},
+    {id:5,label:"تخفیف "},
+    {id:6,label:"قیمت با تخفیف"},
+    {id:7,label:"موجودی "},
+    {id:8,label:"عملیات "},
+   
+ 
+]
+export const categoriesTabelTHead = [
+    {id:1,label:"#"},
+    {id:2,label:"عنوان"},
+    {id:3,label:"توضیحات"},
+    {id:4,label:"عنوتن انگلیسی "},
+    {id:5,label:"نوع "},
+    {id:6,label:"عملیات"},
+   
+   
+ 
+]
--- src/app/hooks/useAuth.js
+++ src/app/hooks/useAuth.js
@@ -1,8 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
-import { getUserProfile } from "../services/AuthServices";
+import { getAllUsers, getUserProfile } from "../services/AuthServices";
 export const useGetUser = () => useQuery({
     queryKey: ["get-user"],
     queryFn: getUserProfile,
     retry: false,
     refetchOnWindowFocus:true
   });
+
+  export const useGetAllUser = () => useQuery({
+    queryKey:["get-user"],
+    queryFn:getAllUsers,
+    retry:false,
+    refetchOnWindowFocus: true
+})
