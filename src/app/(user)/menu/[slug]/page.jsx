aa3330ee01753f20af00cd97ce25a863c17c04ff
Abolghasem Taziki <113670236+AbolghasemTz@users.noreply.github.com>
2023-08-16 15:27:22 +0330

    single page product completed

--- src/app/(user)/menu/[slug]/page.jsx
+++ src/app/(user)/menu/[slug]/page.jsx
@@ -1,43 +1,52 @@
-import { getOneProductsBySlug,getProducts } from '@/src/app/services/ProductServices';
-import AddToCart from './AddToCart';
+import {
+  getOneProductsBySlug,
+  getProducts,
+} from "@/src/app/services/ProductServices";
+import AddToCart from "./AddToCart";
+import { toPersianNumbersWithComma } from "@/utils/toPersianNumber";
+import RoadPage from "./RoadPage";
+import CategrySinglePage from "./CategrySinglePage";
+import RoadPageDescktop from "./RoadPageDescktop";
+import ContentInfo from "./ContentInfo";
 export const dynamic = "force-static";
 export const dynamicParams = false;
 
-async function page({params}) {
-    
-    const {slug} = params;
-    const {product} = await getOneProductsBySlug(slug)
-  return (
-    <div>
-        <h1 className='font-bold text-2xl mb-6'> {product.title}</h1>
-        <p className='mb-6'>{product.description}</p>
-        <p>
-            قیمت محصول :
-            <span className={`${product.discount ? "line-through" : "font-bold"}`}>{product.price}</span>
-        </p>
-        {!!product.discount && (
-            <div className='flex items-center gap-x-2 mb-6'>
-                <p className='text-xl font-bold'>
-                    قیمت با تخفیف : {product.offPrice}
-                </p>
-                <div className='bg-rose-500 px-2 py-0.5 rounded-xl text-white text-sm'>
-                    {product.discount} %
-                </div>
-            </div>
-        )}
+async function page({ params }) {
+  const { slug } = params;
+  const { product } = await getOneProductsBySlug(slug);
 
-       <AddToCart product={product}/>
-     
-    
+  return (
+    <div classNameName="">
+      <RoadPage product={product} />
+
+      <div className="md:px-4 md:mt-6 lg:px-6 container mx-auto max-w-screen-2xl grid grid-cols-12 md:grid-rows-[55px_minmax(500px,_1fr)] md:gap-8">
+        <CategrySinglePage />
+        <RoadPageDescktop product={product} />
+
+        <ContentInfo product={product} />
+      </div>
+
+      {/* sticky bottom in mobile */}
+      <div className="md:hidden fixed flex items-center justify-between left-0 right-0 bottom-0 w-full bg-white shadow-[0_-4px_8px_0_rgba(0,0,0,0.1)] px-4 py-2 gap-x-4">
+        <AddToCart product={product} />
+        <div className="text-slate-800 flex flex-col items-end">
+          <span className="font-bold">
+            {product.offPrice
+              ? toPersianNumbersWithComma(product.offPrice)
+              : toPersianNumbersWithComma(product.price)}
+          </span>
+          <span className="text-gray-400 text-sm">تومان</span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
 
 export async function generateStaticParams() {
-const {products} = await getProducts()
-    return products.map((product) => ({
-        slug: product.slug
-    }))
+  const { products } = await getProducts();
+  return products.map((product) => ({
+    slug: product.slug,
+  }));
 }
